40b2f75672f06e0c8483e6c2f6d37993a881d870
romanornr <romanornr@gmail.com>
2024-02-22 16:00:04 +0100

    Fix gct log setup

--- main.go
+++ main.go
@@ -13,16 +13,34 @@ const (
 	DefaultConfigPath = "config.json"
 )
 
-func init() {
-	initLogger()
+func main() {
+	if err := initLogger(); err != nil {
+		log.Fatalf("Failed to initialise logger. Error: %s", err)
+	}
+
+	// Load the configuration file
+	configFile := util.ConfigFile(DefaultConfigPath)
+
+	app, err := NewBotApplication(&engine.Settings{ConfigFile: configFile})
+	if err != nil {
+		log.Fatalf("Failed to create bot application. Error: %s", err)
+	}
+
+	if err := app.Start(); err != nil {
+		log.Fatalf("Failed to start bot application. Error: %s", err)
+	}
+
+	defer app.Stop()
+	go signaler.WaitForInterrupt()
 }
 
-func initLogger() {
+func initLogger() error {
 	var err error
 	if err == gctlog.SetupGlobalLogger("Delta-Works", true) {
-		log.Fatalf("Failed to setup Global Logger. Error: %s", err)
+		return err
 	}
 	gctlog.Debugf(gctlog.Global, "Logger initialised.")
+	return nil
 }
 
 type BotApplication struct {
@@ -40,31 +58,12 @@ func NewBotApplication(settings *engine.Settings) (*BotApplication, error) {
 func (b *BotApplication) Start() error {
 	b.Bot.Settings.PrintLoadedSettings()
 	if err := b.Bot.Start(); err != nil {
-		errClose := gctlog.CloseLogger()
-		if errClose != nil {
-			log.Printf("Failed to close logger. Error: %s", errClose)
+		if errClose := gctlog.CloseLogger(); errClose != nil {
+			log.Fatalf("Failed to close logger. Error: %s", errClose)
 		}
-		log.Fatalf("Failed to start engine. Error: %s", err)
 		return err
 	}
 	return nil
-}
-
-func main() {
-	// Load the configuration file
-	configPath := util.ConfigFile(DefaultConfigPath)
-
-	app, err := NewBotApplication(&engine.Settings{ConfigFile: configPath})
-	if err != nil {
-		log.Fatalf("Failed to create bot application. Error: %s", err)
-	}
-
-	if err := app.Start(); err != nil {
-		log.Fatalf("Failed to start bot application. Error: %s", err)
-	}
-
-	defer app.Stop()
-	go signaler.WaitForInterrupt()
 }
 
 func (b *BotApplication) Stop() {
